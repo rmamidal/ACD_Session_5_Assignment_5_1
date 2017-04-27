16cc7b264ec96967740503d9d51fc4461fd3bf01
Rajendhar Mamidala <rajendhar.mamidala@capgemini.com>
2017-04-27 10:30:09 +0530

    Created JS file

---
+++ app.js
@@ -0,0 +1,14 @@
+// Daclaring variables.
+var name, age, dateOfBirth, birthPlace;
+
+// Defining variables.
+name = 'Rajendhar Mamidala';
+age = '31';
+dateOfBirth = '25/10/1985';
+birthPlace = 'Andhra Pradesh';
+
+// Printing varible to console.
+console.log(name);
+console.log(age);
+console.log(dateOfBirth);
+console.log(birthPlace);
